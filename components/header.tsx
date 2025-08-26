9daf838e5dffb5df92acb0313eba3d8fd590cd45
Irwing <86335744+Irwingduran@users.noreply.github.com>
2025-08-25 18:44:45 -0600

    Update header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -30,7 +30,7 @@ export function Header() {
                 alt="CardioCentro Cuenca Logo" 
                 width={400} 
                 height={200} 
-                className="h-15 w-35 object-contain"
+                className="h-20 w-35 object-contain"
               />
             </div>
           </div>
